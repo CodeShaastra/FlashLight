8393497e7bfb12037c7fab9ee0b4bf10dff086c7
CodeShaastra <codeshaastra@gmail.com>
2014-10-11 16:09:15 +0530

    added flash light plugin
    added flash light plugin

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -6,13 +6,21 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.flashlight/www/Flashlight.js",
+        "id": "nl.x-services.plugins.flashlight.Flashlight",
+        "clobbers": [
+            "window.plugins.flashlight"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.device": "0.2.12",
-    "org.apache.cordova.console": "0.2.11"
+    "org.apache.cordova.console": "0.2.11",
+    "nl.x-services.plugins.flashlight": "2.0.2"
 }
 // BOTTOM OF METADATA
 });
